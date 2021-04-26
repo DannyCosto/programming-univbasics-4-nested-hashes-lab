ecda0c6767d33b1085b5d69646b6abcddde4da6d
Danny <danny.costelloe@gmail.com>
2021-04-26 15:15:13 -0400

    Done

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,7 +37,8 @@ def alan_kay_is_known_for
       }
     }
 
-end
+    programmer_hash[:alan_kay][:known_for]
+  end
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
+
 
 end
 
@@ -82,6 +85,11 @@ def adding_matz
       }
     }
 
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    programmer_hash
+
 end
 
 def changing_alan
@@ -104,6 +112,8 @@ def changing_alan
       }
     }
 
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +135,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][1] = ["Assembly"]
+    programmer_hash
+
 
 end
